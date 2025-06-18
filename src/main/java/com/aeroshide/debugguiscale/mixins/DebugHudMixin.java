738b0c11f80a4dd16962c1a070f9b479b988707d
MicrocontrollersDev <microcontrollersyt@gmail.com>
2025-06-18 13:34:06 -0700

    convert head and tail injects into a wrapmethod

--- src/main/java/com/aeroshide/debugguiscale/mixins/DebugHudMixin.java
+++ src/main/java/com/aeroshide/debugguiscale/mixins/DebugHudMixin.java
@@ -1,13 +1,12 @@
 package com.aeroshide.debugguiscale.mixins;
 
-import com.aeroshide.debugguiscale.DebugguiscaleClient;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.DebugHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -15,20 +14,16 @@ import static com.aeroshide.debugguiscale.DebugguiscaleClient.scale;
 
 @Mixin(DebugHud.class)
 abstract class DebugHudMixin {
-
-    @Inject(method = "drawText", at = @At("HEAD"))
-    private void beforeTextRendering(DrawContext context, List<String> text, boolean left, CallbackInfo ci) {
+    @WrapMethod(method = "drawText")
+    private void wrapTextRendering(DrawContext context, List<String> text, boolean left, Operation<Void> original) {
         context.getMatrices().push();
         context.getMatrices().scale(scale.floatValue(), scale.floatValue(), 0.0F);
+        original.call(context, text, left);
+        context.getMatrices().pop();
     }
 
     @ModifyExpressionValue(method = "drawText", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;getScaledWindowWidth()I"))
     private int getReadWidth(int original) {
         return (int) (original / scale);
-    }
-
-    @Inject(method = "drawText", at = @At("TAIL"))
-    private void afterTextRendering(DrawContext context, List<String> text, boolean left, CallbackInfo ci) {
-        context.getMatrices().pop();
     }
 }
